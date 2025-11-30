8fca34830b4303bd82c1bcf7e812916eed409505
Your Name <k.bristi057@gmail.com>
2025-11-30 09:28:13 +0600

    added the graph in admin dashboard for transaction

--- src/Utils/navigateRoute.ts
+++ src/Utils/navigateRoute.ts
@@ -1,15 +1,13 @@
 import { type NavigateFunction } from "react-router";
 
-export const navigateToProfile = (
-  navigate: NavigateFunction,
-  role: string
-) => {
+export const navigateToProfile = (navigate: NavigateFunction, role: string) => {
   switch (role) {
     case "ADMIN":
       return navigate("/admin/profile");
     case "AGENT":
       return navigate("/agent/profile");
     case "USER":
+      return navigate("/user/profile");
     default:
       return navigate("/profile");
   }
